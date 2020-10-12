536cc0159f51567a98fbb62a5c5af3408e2a223e
Marcos henrique moreno klein <marcoshklein@gmail.com>
2020-10-12 12:55:37 -0300

    build

---
+++ build/precache-manifest.280053b73159b9a272b66b72749af789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a75f4ba5d0ae85da489ac3f4b7f11a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ec9a3552e6bf377fcaf",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f7b30868d8aa9e07c669",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0ec9a3552e6bf377fcaf",
+    "url": "./static/js/2.c6538a75.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "./static/js/2.c6538a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b30868d8aa9e07c669",
+    "url": "./static/js/main.ac9e119b.chunk.js"
+  },
+  {
+    "revision": "2d75596b4a0b12453b29",
+    "url": "./static/js/runtime-main.34f0e4e0.js"
+  }
+]);
